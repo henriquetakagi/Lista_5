ea20d675a0812e36e6321b707c78f047995ae7fa
henriquetakagi <henrique.takagi@aluno.senai.br>
2026-05-25 19:48:39 -0400

    Exercicio4

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,16 @@
+import java.math.BigDecimal;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    Integer a = 127;
-    Integer b = 127;
+        BigDecimal produto = new BigDecimal("49.90");
+        BigDecimal desconto = produto.multiply(new BigDecimal("0.05"));
+        BigDecimal valorTotal = produto.multiply(new BigDecimal("3"));
+        BigDecimal descontoTotal = desconto.multiply(new BigDecimal("3"));
+        BigDecimal produtoTotal = valorTotal.subtract(descontoTotal);
 
-    System.out.println(a == b);
-
-    Integer c = 200;
-    Integer d = 200;
-
-    System.out.println(c == d);
-    System.out.println(c.equals(d));
+        System.out.println("Produto: "+ produtoTotal);
 
     }
 }
